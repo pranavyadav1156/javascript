a77d9c2ae6483d22d3eabd24af7d7c01fd4c9dec
pranavyadav1156 <124560006+pranavyadav1156@users.noreply.github.com>
2026-04-05 12:11:41 +0000

    variable types

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+const accountId = 144553
+let accountEmail = "pranav@gmail.com"
+var accountPassword = "12345"
+accountCity = "Chennai"
+let accountState
+
+// accountId=5
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword,accountState])
+
+
+//var -  scoped to function its declared in
+//let - scoped to the nearest curly braces {}
+
+//prefer not to use var - because of issue in block scope and functional scope
+
