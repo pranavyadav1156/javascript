283e590aa92e14d1c6792608f6ceb26bccbf07fb
pranavyadav1156 <124560006+pranavyadav1156@users.noreply.github.com>
2026-04-05 12:28:43 +0000

    data_types

---
+++ 01_basics/data_types.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all JS code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+
+//number = 2^53 
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value 
+//undefined 
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Pranav")
+console.log(typeof null)
+console.log(typeof undefined)
+
